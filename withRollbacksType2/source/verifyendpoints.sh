511e909f685b8b69cc32045d2ed418e128382b97
Saifur Rehman Khan <43892879+Saifu0@users.noreply.github.com>
2022-02-25 10:53:09 +0530

    moved items from withRollbacksType2/failedWithNoRollbacks to withRollbacksType2/source

--- withRollbacksType2/source/verifyendpoints.sh
+++ withRollbacksType2/source/verifyendpoints.sh
@@ -3,12 +3,13 @@
 sleep 10
 for i in 1 2 3 4 5
 do
-   echo $SERVICE_INTERNAL_URL
-   resp=$(curl $SERVICE_INTERNAL_URL:5000/)
+   echo $SERVICE_INTERNAL_UR
+   resp=$(curl $SERVICE_INTERNAL_UR:5000/)
    if [ "$resp" != "Hello, World!" ]; then
       echo "Did not return correctly."
       exit 1;
    fi
    sleep 1
 done
+exit 1
 echo "Test completed correctly."
